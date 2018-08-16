806965ec12a4aa864f6651fb88deb2dfe26caddf
lucasisu <luiscarlossilvasuarez@gmail.com>
2018-08-16 10:43:44 -0500

    Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-10.times do
-Report.create({
-title: Faker:Book.title
-body: Faker::Lorem.sentence
+
+PersonType.create({
+	person_type: "admin"
+	
 })
+
+
+
+10.times do
+
 end
